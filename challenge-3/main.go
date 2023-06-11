9f3ab7dac179822b8b3852b2c78b4b62461b4821
theemadnes <alex.mattson@gmail.com>
2023-06-11 03:38:35 +0000

    basic 3 working

---
+++ challenge-3/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	var messages []float64
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// store message
+		messages = append(messages, body["message"].(float64))
+
+		// Update the message type to return back.
+		body["type"] = "broadcast_ok"
+		delete(body, "message")
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "read_ok"
+		body["messages"] = messages
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "topology_ok"
+		delete(body, "topology")
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
